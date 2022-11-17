fb4d92c1c6f11f7c6d8b1357c73841cb91d33424
Connor <connor.cs@protonmail.com>
2022-11-17 12:54:37 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1. Two Sum
+
+//brute force solution
+const twoSum = function (nums, target) {
+    for (let i=0; i<nums.length; i++){
+        for (let j=0; j<nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [nums[i], nums[j]]
+            }
+        }
+    }
+}
