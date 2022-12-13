f9e23529194a0f7d72863efd90d6f12cdf41992a
Connor <connor.cs@protonmail.com>
2022-12-13 09:32:15 -0500

    abc

--- index.js
+++ index.js
@@ -9,4 +9,40 @@ const twoSum = function (nums, target) {
             }
         }
     }
+    return null
+}
+
+//hashmap solution
+
+const Twosum = function (nums, target)  {
+    const map={}
+    for (let i=0; i<nums.length; i++) {
+        map[nums[i]] ? 
+    }
+}
+
+//14. Longest Common Prefix
+
+const longestCommonPrefix = function (strs) {
+    if (!strs.length) return ''
+    for (let i=0; i<=strs[0].length; i++) {
+        for (let j=1; j<strs.length; j++) {
+            if (strs[0][i] !== strs[j][i]) {
+                return strs[0].slice(0, i)
+            }
+        }
+    }
+    return strs[0]
+}
+
+// better version user .every()
+
+const longestCommon = function (strs) {
+    if (!strs.length) return ''
+    for (let i=0; i<=strs[0].length; i++) {
+        if (!strs.every(str =>str[i] ===[0][i])) {
+            return strs[0].slice(0,i)
+        }
+    }
+    return strs[0]
 }
