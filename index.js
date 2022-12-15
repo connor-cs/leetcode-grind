b33f19e924e66bb741eaadc2887ff638977ab9f3
Connor <connor.cs@protonmail.com>
2022-12-14 21:39:08 -0500

    another

--- index.js
+++ index.js
@@ -2,47 +2,60 @@
 
 //brute force solution
 const twoSum = function (nums, target) {
-    for (let i=0; i<nums.length; i++){
-        for (let j=0; j<nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [nums[i], nums[j]]
-            }
-        }
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [nums[i], nums[j]];
+      }
     }
-    return null
-}
+  }
+  return null;
+};
 
 //hashmap solution
 
-const Twosum = function (nums, target)  {
-    const map={}
-    for (let i=0; i<nums.length; i++) {
-        map[nums[i]] ? 
-    }
-}
+const Twosum = function (nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    map[nums[i]] ? map[nums[i]]++ : (map[nums[i]] = 1);
+  }
+};
 
 //14. Longest Common Prefix
 
 const longestCommonPrefix = function (strs) {
-    if (!strs.length) return ''
-    for (let i=0; i<=strs[0].length; i++) {
-        for (let j=1; j<strs.length; j++) {
-            if (strs[0][i] !== strs[j][i]) {
-                return strs[0].slice(0, i)
-            }
-        }
+  if (!strs.length) return "";
+  for (let i = 0; i <= strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) {
+        return strs[0].slice(0, i);
+      }
     }
-    return strs[0]
-}
+  }
+  return strs[0];
+};
 
 // better version user .every()
 
 const longestCommon = function (strs) {
-    if (!strs.length) return ''
-    for (let i=0; i<=strs[0].length; i++) {
-        if (!strs.every(str =>str[i] ===[0][i])) {
-            return strs[0].slice(0,i)
-        }
+  if (!strs.length) return "";
+  for (let i = 0; i <= strs[0].length; i++) {
+    if (!strs.every((str) => str[i] === [0][i])) {
+      return strs[0].slice(0, i);
     }
-    return strs[0]
+  }
+  return strs[0];
+};
+
+//26. Remove Duplicates from Sorted Array
+
+function removeDuplicates(nums) {
+  let k = 1;
+  for (let j=1; j < nums.length; j++) {
+    if (nums[j - 1] != nums[j]) {
+      nums[k] = nums[j];
+      k++;
+    }
+  }
+  return k;
 }
